c2285a8413342072ccd19636fa62aa6604a9374d
Eduard Tif <eduard.tif@stud.fh-campuswien.ac.at>
2022-04-29 19:40:24 +0200

    Step one

--- src/main/java/stockanalyzer/ctrl/Controller.java
+++ src/main/java/stockanalyzer/ctrl/Controller.java
@@ -1,5 +1,13 @@
 package stockanalyzer.ctrl;
 
+import yahooApi.YahooFinance;
+import yahooApi.beans.QuoteResponse;
+import yahooApi.beans.YahooResponse;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class Controller {
 		
 	public void process(String ticker) {
@@ -10,12 +18,17 @@ public class Controller {
 		//TODO implement methods for
 		//1) Daten laden
 		//2) Daten Analyse
-
+		getData(ticker);
 	}
 	
 
 	public Object getData(String searchString) {
 
+		List<String> tickerString = Arrays.asList(searchString);
+		YahooFinance yahooFinance = new YahooFinance();
+		YahooResponse yahooResponse = yahooFinance.getCurrentData(tickerString);
+		QuoteResponse quoteResponse = yahooResponse.getQuoteResponse();
+		quoteResponse.getResult().stream().forEach(quote -> System.out.println(quote.getAsk()));
 		
 		return null;
 	}
